8319990562acbb4d70e277b46a1f4eed3cc800d5
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-26 15:44:59 +0100

    2 esempio di codifica

--- unicode/Program.cs
+++ unicode/Program.cs
@@ -76,6 +76,46 @@ namespace unicode
 			//}
 			//Console.WriteLine($"{numeroConvertito}");
 			//Console.ReadLine();
+
+
+
+			// Secondo es permette di calcolare il peso di una immagine 
+			//string risultato;
+			//double calcolo = 0;
+			//Console.WriteLine("Inserisci il primo numero");
+			//double n1 = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Inserisci il secondo numero");
+			//double n2 = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Inserisci la grandezza 1, 8, 16, 24");
+			//risultato = Console.ReadLine();
+			//switch (risultato)
+			//{
+			//	case "1":
+			//		calcolo = ((n1 * n2) / 8) / 1024;
+			//		Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+			//		break;
+
+			//	case "8":
+			//		calcolo = (n1 * n2) / 1024;
+			//		Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+			//		break;
+
+			//	case "16":
+			//		calcolo = ((n1 * n2) * 2) / 1024;
+			//		Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+			//		break;
+
+			//	case "24":
+			//		calcolo = ((n1 * n2) * 3) / 1024;
+			//		Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+			//		break;
+
+			//	default:
+			//		Console.WriteLine(" Attenzione questo codice vale per 1, 8 16, 24, inserire un numero corretto");
+			//		break;
+			//}
+			//Console.ReadLine();
+
 		}
 	}
 }

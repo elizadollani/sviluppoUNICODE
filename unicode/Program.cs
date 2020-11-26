b378ae6242c50d07468e1cc391d7868cf56d751a
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-26 15:39:35 +0100

    Aggiungere i file di progetto.

---
+++ unicode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace unicode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//string unicodeString =
+			//	 "This unicode string has 2 characters outside the" +
+			//	  " ASCII range: \n" +
+			//	  "Pi (\u03A0), and Sigma (\u03A3).";
+			string unicodeString = "";
+			Console.WriteLine("Original String");
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			Console.WriteLine(unicodeString);
+			Console.WriteLine();
+		}
+	}
+}

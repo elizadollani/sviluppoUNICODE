98d96aa91a6af9dde5340351e3967b422cfdb2f3
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-26 15:44:05 +0100

    1 esempio di codifica

--- unicode/Program.cs
+++ unicode/Program.cs
@@ -18,6 +18,64 @@ namespace unicode
 			Console.OutputEncoding = System.Text.Encoding.Unicode;
 			Console.WriteLine(unicodeString);
 			Console.WriteLine();
+
+			// Primo esempio di un esercizio 
+			// Permette di trasformare qualsiasi numero da decimale a binario, ottale ed esadecimale 
+
+			//int numero;
+			//int resto;
+			//string numeroConvertito = "";
+			//int divisore;
+			//resto = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Inserisci un numero:");
+			//numero = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Inserisci un divisore");
+			//divisore = Convert.ToInt32(Console.ReadLine());
+			//while (numero > 0)
+			//{
+			//	resto = numero % divisore;
+			//	numero = numero / 2;
+			//	numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+			//}
+			//while (numero > 0)
+			//{
+			//	resto = numero % divisore;
+			//	numero = numero / 8;
+			//	numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+			//}
+			//while (numero > 0)
+			//{
+			//	if (resto == 10)
+			//	{
+			//		numeroConvertito = "A" + numeroConvertito;
+			//	}
+			//	else if (resto == 11)
+			//	{
+			//		numeroConvertito = "B" + numeroConvertito;
+			//	}
+			//	else if (resto == 12)
+			//	{
+			//		numeroConvertito = "C" + numeroConvertito;
+			//	}
+			//	else if (resto == 13)
+			//	{
+			//		numeroConvertito = "D" + numeroConvertito;
+			//	}
+			//	else if (resto == 14)
+			//	{
+			//		numeroConvertito = "E" + numeroConvertito;
+			//	}
+			//	else if (resto == 15)
+			//	{
+			//		numeroConvertito = "F" + numeroConvertito;
+			//	}
+			//	else
+			//	{
+			//		numeroConvertito = Convert.ToString(resto);
+			//	}
+			//}
+			//Console.WriteLine($"{numeroConvertito}");
+			//Console.ReadLine();
 		}
 	}
 }

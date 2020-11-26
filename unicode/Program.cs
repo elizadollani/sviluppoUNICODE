6e1a894b2842f9eb1f06934e9e31cdd5421a7ea0
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-26 15:41:23 +0100

    Inserimento frase a tastiera + commento

--- unicode/Program.cs
+++ unicode/Program.cs
@@ -11,6 +11,8 @@ namespace unicode
 			//	  " ASCII range: \n" +
 			//	  "Pi (\u03A0), and Sigma (\u03A3).";
 			string unicodeString = "";
+			Console.WriteLine(" Scrivi un mesaggio"); // Dico all' utente di inserire una qualsiasi frase a tastiera 
+			unicodeString = Console.ReadLine();  // permette di inserire la frase all'utente 
 			Console.WriteLine("Original String");
 			Console.OutputEncoding = System.Text.Encoding.Unicode;
 			Console.WriteLine(unicodeString);
